f9d647ed78f58a7192b0c03d7e497b2e53f6b281
Tatsuro Yokoyama <tyinnagoyauniversity@gmail.com>
2022-06-18 18:18:59 +0900

    add domain

--- myapp/settings.py
+++ myapp/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["160.251.72.208"]
+ALLOWED_HOSTS = ["predict-ponv.com"]
 
 # Application definition
 
