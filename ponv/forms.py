36d81e83a0d595b9a36fb0078b5fc1bafd3d518d
Tatsuro Yokoyama <tyinnagoyauniversity@gmail.com>
2022-07-11 10:54:05 +0900

    set range

--- ponv/forms.py
+++ ponv/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class ModelForm(forms.Form):
-    age = forms.IntegerField(label='age')
-    sex = forms.IntegerField(label='sex')
-    smoke = forms.IntegerField(label='smoke')
-    ponv_history = forms.IntegerField(label='ponv_history')
-    anxiety = forms.IntegerField(label='anxiety')
+    age = forms.IntegerField(label='age',validators=[MinValueValidator(20), MaxValueValidator(120)])
+    sex = forms.IntegerField(label='sex',validators=[MinValueValidator(0), MaxValueValidator(1)])
+    smoke = forms.IntegerField(label='smoke',validators=[MinValueValidator(0), MaxValueValidator(1)])
+    ponv_history = forms.IntegerField(label='ponv_history',validators=[MinValueValidator(0), MaxValueValidator(1)])
+    anxiety = forms.IntegerField(label='anxiety',validators=[MinValueValidator(0), MaxValueValidator(21)])
